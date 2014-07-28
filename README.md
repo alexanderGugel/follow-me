17c148422812375be179fe2de070a47744f6126e
Alexander Gugel <alexander.gugel@gmail.com>
2014-07-28 11:03:23 -0700

    Init

---
+++ README.md
@@ -0,0 +1,6 @@
+A quick-and-dirty zero-config GitHub spamming ring.
+
+## How to
+```
+node spam
+```
---
+++ spam.js
@@ -0,0 +1,56 @@
+var prompt = require('prompt');
+var GitHubApi = require('github');
+var _ = require('lodash');
+
+prompt.start();
+
+var github = new GitHubApi({
+  version: '3.0.0',
+  timeout: 5000
+});
+
+prompt.get([{
+  name: 'username'
+}, {
+  name: 'password',
+  hidden: true
+}], function (err, loginCredentials) {
+  github.authenticate({
+    type: 'basic',
+    username: loginCredentials.username,
+    password: loginCredentials.password
+  });
+
+  // Always follow ME first!
+  github.user.followUser({
+    user: 'alexanderGugel'
+  });
+
+  // Follow all Hack Reactor students
+  github.orgs.getMembers({
+    org: 'hackreactor',
+    per_page: 100
+  }, function (err, res) {
+    console.log(res);
+    _.each(res, function (user) {
+      console.log(user.login);
+      github.user.followUser({
+        user: user.login
+      });
+    });
+  });
+
+  github.orgs.getMembers({
+    org: 'hackreactor',
+    per_page: 100,
+    page: 2
+  }, function (err, res) {
+    console.log(res);
+    _.each(res, function (user) {
+      console.log(user.login);
+      github.user.followUser({
+        user: user.login
+      });
+    });
+  });
+});
